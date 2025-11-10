41ae15d161db0d4f968306f9f5a84d211ef4cae3
Fredrik Roos <fredrik@stensatter.se>
2025-11-10 19:09:59 +0100

    ct

--- cgi-bin/img.c
+++ cgi-bin/img.c
@@ -8,16 +8,14 @@ char* getenv(const char* name);
 int main(int args, char* argv[]){
 
 
-   printf("Content-Encoding: gzip\n");
-   printf("Content-Type: image/png\n\n");
+   fprintf(stdout,"Content-Type: image/png\n\n");
 
 
    char* buffer = NULL;
-   FILE* fd = fopen("/home/nrkfrr/GIT/corneliaws/www/corn2.png","rb");
+   FILE* fd = fopen("/home/nrkfrr/GIT/corneliaws/www/images/corn2.png","rb");
    if(fd!=NULL){
      fseek(fd,0,SEEK_END);
      int size = ftell(fd);
-//     printf("%d\n",size);
      fseek(fd,0,SEEK_SET);
      buffer = (char*)malloc(size);
      fread(buffer,1,size,fd);
